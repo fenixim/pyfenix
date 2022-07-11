434d18acfb7729f7a867e7dae2a598ef2d7f61c3
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2022-07-11 12:51:47 -0500

    document event types

--- client/fenix/event.py
+++ client/fenix/event.py
@@ -3,7 +3,14 @@
 import enum
 
 class Event(enum.Enum):
-    """Enumeration of all events. The ordering should not be relied on."""
+    """
+    Enumeration of all events. The ordering should not be relied on.
+
+    CONN_FAIL indicates a failure to connect to the Fenix server
+    MSG_RECV indicates an incoming message event
+    MSG_SEND indicates an outgoing message event
+    QUIT should be used to terminate all threads cleanly
+    """
     CONN_FAIL = enum.auto()
     MSG_RECV = enum.auto()
     MSG_SEND = enum.auto()
